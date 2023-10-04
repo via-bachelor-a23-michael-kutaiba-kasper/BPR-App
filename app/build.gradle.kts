051cc92b8809202fb65316aba73be7e0289df568
Michael Bui <michaelbui99@hotmail.com>
2023-10-04 13:54:53 +0200

    Use same version of dagger

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ apollo {
     }
 }
 kotlin {
-    jvmToolchain(8)
+    jvmToolchain(17)
 }
 android {
     namespace = "io.github.viabachelora23michaelkutaibakasper.bprapp"
@@ -35,7 +35,7 @@ android {
         }
 
         kotlinOptions {
-            jvmTarget = "1.8"
+            jvmTarget = "17"
         }
     }
 
@@ -46,12 +46,10 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+
     buildFeatures {
         compose = true
     }
--- build.gradle.kts
+++ build.gradle.kts
@@ -17,5 +17,5 @@ buildscript {
 plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
-    id("com.google.dagger.hilt.android") version "2.44" apply false
+    id("com.google.dagger.hilt.android") version "2.42" apply false
 }
