5e8c4a5267c56406143222ab2129a5d6fdf503f6
Kasper-S-Jensen <kasperscoop@gmail.com>
2023-10-18 14:57:10 +0200

    add top level navigation and tabview for events on map and in list view

--- app/src/main/java/io/github/viabachelora23michaelkutaibakasper/bprapp/MainActivity.kt
+++ app/src/main/java/io/github/viabachelora23michaelkutaibakasper/bprapp/MainActivity.kt
@@ -4,28 +4,18 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Build
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.Face
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Star
-import androidx.compose.material3.BottomAppBar
+import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -38,27 +28,24 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import io.github.viabachelora23michaelkutaibakasper.bprapp.ui.screens.events.MapListView
 import io.github.viabachelora23michaelkutaibakasper.bprapp.ui.screens.events.MapView
 import io.github.viabachelora23michaelkutaibakasper.bprapp.ui.theme.BPRAppTheme
 
 class MainActivity : ComponentActivity() {
     enum class Screens() {
         Map,
+        Recommendations,
         Achievements,
         Profile
     }
@@ -112,8 +99,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
     val navController: NavHostController = rememberNavController()
-    var presses by remember { mutableIntStateOf(0) }
-    val context = LocalContext.current
     Scaffold(
         topBar = {
             TopAppBar(
@@ -123,7 +108,6 @@ fun MainScreen(modifier: Modifier = Modifier) {
                             Icon(Icons.AutoMirrored.Filled.ArrowBack, "backIcon")
                         }
                     }
-
                 },
                 title = {
                     Text("VibeVerse")
@@ -150,11 +134,25 @@ fun MainScreen(modifier: Modifier = Modifier) {
                             )
                         })
                     NavigationBarItem(
-                        label = { Text("Achievements") },
+                        label = { Text("Recomm.") },
                         selected = (selectedIndex.intValue == 1),
                         onClick = {
                             selectedIndex.intValue =
-                                1; navController.navigate(MainActivity.Screens.Achievements.name)
+                                1; navController.navigate(MainActivity.Screens.Recommendations.name)
+                        },
+                        icon = {
+                            Icon(
+                                imageVector = Icons.Filled.ThumbUp,
+                                contentDescription = "Achievements",
+                                tint = Color.Black,
+                            )
+                        })
+                    NavigationBarItem(
+                        label = { Text("Medals") },
+                        selected = (selectedIndex.intValue == 2),
+                        onClick = {
+                            selectedIndex.intValue =
+                                2; navController.navigate(MainActivity.Screens.Achievements.name)
                         },
                         icon = {
                             Icon(
@@ -165,9 +163,9 @@ fun MainScreen(modifier: Modifier = Modifier) {
                         })
                     NavigationBarItem(
                         label = { Text("Profile") },
-                        selected = (selectedIndex.intValue == 2),
+                        selected = (selectedIndex.intValue == 3),
                         onClick = {
-                            selectedIndex.intValue = 2;
+                            selectedIndex.intValue = 3;
                             navController.navigate(MainActivity.Screens.Profile.name)
                         },
                         icon = {
@@ -179,29 +177,21 @@ fun MainScreen(modifier: Modifier = Modifier) {
                         })
                 }
             )
-        },
-        floatingActionButton = {
-            FloatingActionButton(onClick = {
-                presses++
-            }) {
-                Column {
-                    Icon(Icons.Default.Build, contentDescription = "Add")
-                    Text(text = presses.toString())
-                }
-            }
         }
     ) { innerPadding ->
-
         NavHost(
             navController = navController,
             startDestination = MainActivity.Screens.Map.name,
             modifier = Modifier.padding(innerPadding)
         ) {
             composable(MainActivity.Screens.Map.name) {
-                MapView().Map()
+                MapView().Map(navController = navController)
+            }
+            composable(MainActivity.Screens.Recommendations.name) {
+                Text(text = "Recommendations")
             }
             composable(MainActivity.Screens.Achievements.name) {
-                MapListView().EventList()
+                Text(text = "Achievements")
             }
             composable(MainActivity.Screens.Profile.name) {
                 Text(text = "Profile")
