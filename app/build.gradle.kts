294f52cecdb6e7e97a7d1e3feceb731ba4cdfe58
Kasper-S-Jensen <kasperscoop@gmail.com>
2023-10-26 18:45:14 +0200

    update dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
     defaultConfig {
         applicationId = "io.github.viabachelora23michaelkutaibakasper.bprapp"
         minSdk = 27
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
