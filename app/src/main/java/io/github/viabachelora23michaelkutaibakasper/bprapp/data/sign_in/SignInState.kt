9f18bcd4306fd95acb4e2f6f23cf618af7aa5247
Kasper-S-Jensen <kasperscoop@gmail.com>
2023-10-29 13:01:10 +0100

    remove unused classes

--- app/src/main/java/io/github/viabachelora23michaelkutaibakasper/bprapp/data/sign_in/SignInState.kt
+++
@@ -1,6 +0,0 @@
-package io.github.viabachelora23michaelkutaibakasper.bprapp.data.sign_in
-
-data class SignInState(
-    val isSignInSuccessful:Boolean = false,
-    val errorMessage: String? = null
-)
