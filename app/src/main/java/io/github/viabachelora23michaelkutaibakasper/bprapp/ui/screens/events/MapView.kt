2a974235719ce658802836980826eb02f5ef64d2
Kasper-S-Jensen <kasperscoop@gmail.com>
2023-11-08 14:52:33 +0100

    add initial cluster functionality

--- app/src/main/java/io/github/viabachelora23michaelkutaibakasper/bprapp/ui/screens/events/MapView.kt
+++ app/src/main/java/io/github/viabachelora23michaelkutaibakasper/bprapp/ui/screens/events/MapView.kt
@@ -1,10 +1,10 @@
+@file:OptIn(MapsComposeExperimentalApi::class)
+
 package io.github.viabachelora23michaelkutaibakasper.bprapp.ui.screens.events
 
 import android.content.Intent
 import android.net.Uri
 import android.util.Log
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,7 +15,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Build
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Place
 import androidx.compose.material3.Button
@@ -23,36 +22,43 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.PrimaryTabRow
 import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.clustering.ClusterItem
+import com.google.maps.android.clustering.ClusterManager
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.MapsComposeExperimentalApi
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.clustering.Clustering
 import com.google.maps.android.compose.rememberCameraPositionState
+import io.github.viabachelora23michaelkutaibakasper.bprapp.BottomNavigationScreens
+import io.github.viabachelora23michaelkutaibakasper.bprapp.CreateEventScreens
+import io.github.viabachelora23michaelkutaibakasper.bprapp.MainActivity
 import io.github.viabachelora23michaelkutaibakasper.bprapp.data.domain.Event
 import io.github.viabachelora23michaelkutaibakasper.bprapp.ui.theme.BPRAppTheme
 
@@ -84,7 +90,7 @@ class MapView {
 
             when (selectedIndex.intValue) {
                 mapView -> {
-                    mapEvents()
+                    MapEvents(navController = navController)
                 }
 
                 listView -> {
@@ -107,9 +113,8 @@ class MapView {
 
         if (isLoading) {
             LoadingScreen()
-        } else if (response.isEmpty()) {
-            LoadingScreen() //should display a No Events message
-        } else {
+        } //should display a No Events message
+        else {
             LazyColumn {
                 items(response) { event ->
                     Column {
@@ -141,10 +146,15 @@ class MapView {
 }
 
 @Composable
-fun mapEvents() {
+fun MapEvents(navController: NavController) {
     val horsens = LatLng(55.862207, 9.844651)
+    val horsens1 = LatLng(55.862207, 10.844651)
+    val horsens2 = LatLng(55.862207, 11.844651)
+    val horsens3 = LatLng(55.862207, 12.844651)
+    val horsens4 = LatLng(55.862207, 13.844651)
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(horsens, 15f)
+
     }
     val uiSettings by remember {
         mutableStateOf(
@@ -157,6 +167,7 @@ fun mapEvents() {
             )
         )
     }
+
     Box(modifier = Modifier.fillMaxSize()) {
         GoogleMap(
             modifier = Modifier.fillMaxSize(),
@@ -185,10 +196,49 @@ fun mapEvents() {
                     snippet = event.description ?: "No description",
                 )
             }
-        }
+            val clusterEvents = remember {
+                mutableStateListOf(
+                    EventClusterItem(
+                        lat = 55.862207,
+                        lng = 9.345575,
+                        title = "Event 1",
+                        snippet = "Event 1"
+                    ),
+                    EventClusterItem(
+                        lat = 55.862207,
+                        lng = 9.344654,
+                        title = "Event 2",
+                        snippet = "Event 2"
+                    ),
+                    EventClusterItem(
+                        lat = 55.862207,
+                        lng = 9.346653,
+                        title = "Event 3",
+                        snippet = "Event 3"
+                    ),
+                    EventClusterItem(
+                        lat = 55.862207,
+                        lng = 9.341652,
+                        title = "Event 4",
+                        snippet = "Event 4"
+                    ),
+                    EventClusterItem(
+                        lat = 55.862207,
+                        lng = 9.342651,
+                        title = "Event 5",
+                        snippet = "Event 5"
+                    )
+                )
 
+            }
+            Clustering(
+                items = clusterEvents,
+            )
+        }
         FloatingActionButton(
-            onClick = {}, content = {
+            onClick = {
+                navController.navigate(CreateEventScreens.Title.name)
+            }, content = {
                 Column {
                     Icon(Icons.Default.Add, contentDescription = "Add")
                 }
@@ -199,6 +249,40 @@ fun mapEvents() {
         )
     }
 
+}
+
+private class EventClusterItem(
+    lat: Double,
+    lng: Double,
+    title: String,
+    snippet: String
+) : ClusterItem {
+
+    private val position: LatLng
+    private val title: String
+    private val snippet: String
+
+    override fun getPosition(): LatLng {
+        return position
+    }
+
+    override fun getTitle(): String {
+        return title
+    }
+
+    override fun getSnippet(): String {
+        return snippet
+    }
+
+    override fun getZIndex(): Float? {
+        return 0f
+    }
+
+    init {
+        position = LatLng(lat, lng)
+        this.title = title
+        this.snippet = snippet
+    }
 }
 
 @Composable
