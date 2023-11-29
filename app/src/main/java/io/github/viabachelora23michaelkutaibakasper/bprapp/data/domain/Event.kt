62be4b6264b6118aaee7b009bc35fff17e6fb7e4
Kasper-S-Jensen <kasperscoop@gmail.com>
2023-11-29 09:11:45 +0100

    Update event domain class

--- app/src/main/java/io/github/viabachelora23michaelkutaibakasper/bprapp/data/domain/Event.kt
+++ app/src/main/java/io/github/viabachelora23michaelkutaibakasper/bprapp/data/domain/Event.kt
@@ -1,13 +1,33 @@
 package io.github.viabachelora23michaelkutaibakasper.bprapp.data.domain
 
+import android.net.Uri
+import java.time.LocalDateTime
+import java.util.Date
+
 data class Event(
 
     val title: String?,
     val description: String?,
     val url: String?,
-    val location: Location?
+    val location: Location?,
+    val type: String?,
+    val isPrivate: Boolean?,
+    val isPaid: Boolean?,
+    val isAdultsOnly: Boolean?,
+    val selectedStartDateTime: LocalDateTime?,
+    val selectedEndDateTime: LocalDateTime?,
+    val selectedKeywords: List<String>?,
+    val selectedCategory: String?,
+    val maxNumberOfAttendees: Int?,
+    val host:User?,
+    val lastUpdatedDate: LocalDateTime?,
+    val photos : List<String>?
 )
-
+data class User(
+    var displayName:String,
+    var userId:String,
+    var photoUrl:Uri?
+)
 data class Location(
     var city: String?,
     var completeAddress: String?,
