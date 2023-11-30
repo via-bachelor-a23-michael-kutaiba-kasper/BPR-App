4fbe0a4b87a269465a9020036f8744602341af62
Kasper-S-Jensen <kasperscoop@gmail.com>
2023-11-30 16:01:35 +0100

    add endpoints for fetching all events

--- app/src/main/java/io/github/viabachelora23michaelkutaibakasper/bprapp/data/repository/EventRepository.kt
+++ app/src/main/java/io/github/viabachelora23michaelkutaibakasper/bprapp/data/repository/EventRepository.kt
@@ -1,71 +1,113 @@
 package io.github.viabachelora23michaelkutaibakasper.bprapp.data.repository
 
+import android.net.Uri
 import android.util.Log
 import com.apollographql.apollo3.ApolloClient
 import io.github.viabachelora23michaelkutaibakasper.bprapp.BuildConfig
 import io.github.viabachelora23michaelkutaibakasper.bprapp.CreateEventMutation
+import io.github.viabachelora23michaelkutaibakasper.bprapp.FetchAllEventsQuery
 import io.github.viabachelora23michaelkutaibakasper.bprapp.GetCategoriesQuery
+import io.github.viabachelora23michaelkutaibakasper.bprapp.GetEventQuery
 import io.github.viabachelora23michaelkutaibakasper.bprapp.GetKeywordsQuery
 import io.github.viabachelora23michaelkutaibakasper.bprapp.data.domain.Event
+import io.github.viabachelora23michaelkutaibakasper.bprapp.data.domain.User
 import io.github.viabachelora23michaelkutaibakasper.bprapp.type.GeoLocationInput
 import io.github.viabachelora23michaelkutaibakasper.bprapp.type.UserInput
+import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 
 class EventRepository : IEventRepository {
-    override suspend fun getEvents(): List<Event> {
-        /* val apolloClient = ApolloClient.Builder()
-             .serverUrl(BuildConfig.API_URL)
-             .build()
-         val response = apolloClient.query(AllPublicEventsQuery()).execute()
-         Log.d("ApolloEventClient", "getPublicEvents: ${response.data?.allPublicEvents}")
-         if (response.hasErrors()) Log.d("ApolloEventClient", "getPublicEvents: ${response.errors}")
-         return response.data?.allPublicEvents?.map {
-             Event(
-                 title = it?.title,
-                 description = it?.description,
-                 url = it?.url,
-                 location = Location(
-                     city = it?.location?.city,
-                     completeAddress = it?.title,
-                     geoLocation = GeoLocation(
-                         lat = it?.location?.geoLocation?.lat!!.toDouble(),
-                         lng = it?.location?.geoLocation?.lng!!.toDouble()
-                     )
-                 ),
-                 type = null,
-                 isPrivate = null,
-                 isPaid = null,
-                 isAdultsOnly = null,
-                 selectedStartDateTime = null,
-                 selectedEndDateTime = null,
-                 selectedKeywords = null,
-                 selectedCategory = null,
-                 maxNumberOfAttendees = null,
-                 host = User(
-                     displayName = "Michael Kuta Ibaka",
-                     userId = "123456789",
-                     photoUrl = null
-                 ),
-                 lastUpdatedDate = null,
-                 emptyList()
-             )
-         } ?: emptyList()*/
-        return emptyList()
+    fun parseUtcStringToLocalDateTime(utcString: String): LocalDateTime {
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+        return LocalDateTime.parse(utcString, formatter)
     }
 
-    override suspend fun getEvent(eventId: String): Event {
-        TODO("Not yet implemented")
+    override suspend fun getEvents(): List<Event> {
+        val apolloClient = ApolloClient.Builder()
+            .serverUrl(BuildConfig.API_URL)
+            .build()
+        val response = apolloClient.query(FetchAllEventsQuery()).execute()
+        Log.d("ApolloEventClient", "getEvent UwWU: ${response.data?.events}")
+        return response.data?.events?.map {
+            Event(
+                title = it?.title,
+                description = it?.description,
+                url = it?.url,
+                location = io.github.viabachelora23michaelkutaibakasper.bprapp.data.domain.Location(
+                    city = it?.city,
+                    completeAddress = it?.location,
+                    geoLocation = io.github.viabachelora23michaelkutaibakasper.bprapp.data.domain.GeoLocation(
+                        lat = it?.geoLocation?.lat!!.toDouble(),
+                        lng = it.geoLocation.lng!!.toDouble()
+                    )
+                ),
+                isPrivate = it.isPrivate,
+                isPaid = it.isPaid,
+                isAdultsOnly = it.adultsOnly,
+                selectedStartDateTime = parseUtcStringToLocalDateTime(it.startDate!!),
+                selectedEndDateTime = parseUtcStringToLocalDateTime(it.endDate!!),
+                selectedKeywords = it.keywords,
+                selectedCategory = it.category,
+                maxNumberOfAttendees = it.maxNumberOfAttendees,
+                host = User(
+                    displayName = it.host?.displayName!!,
+                    userId = it.host.userId!!,
+                    photoUrl = it.host.photoUrl?.let { Uri.parse(it) },
+                    creationDate = parseUtcStringToLocalDateTime(it.host.creationDate!!),
+                    lastSeenOnline = parseUtcStringToLocalDateTime(it.host.lastSeenOnline!!)
+                ),
+                lastUpdatedDate = parseUtcStringToLocalDateTime(it.lastUpdateDate!!),
+                photos = it.images ?: emptyList()
+            )
+        } ?: emptyList()
+    }
+
+    override suspend fun getEvent(eventId: Int): Event {
+        val apolloClient = ApolloClient.Builder()
+            .serverUrl(BuildConfig.API_URL)
+            .build()
+        val response = apolloClient.query(GetEventQuery(eventId)).execute()
+        Log.d("ApolloEventClient", "getEvent UwWU: ${response.data?.event}")
+        return Event(
+            title = response.data?.event?.title,
+            description = response.data?.event?.description,
+            url = response.data?.event?.url,
+            location = io.github.viabachelora23michaelkutaibakasper.bprapp.data.domain.Location(
+                city = response.data?.event?.city,
+                completeAddress = response.data?.event?.location,
+                geoLocation = io.github.viabachelora23michaelkutaibakasper.bprapp.data.domain.GeoLocation(
+                    lat = response.data?.event?.geoLocation?.lat!!.toDouble(),
+                    lng = response.data?.event?.geoLocation?.lng!!.toDouble()
+                )
+            ),
+            isPrivate = response.data?.event?.isPrivate,
+            isPaid = response.data?.event?.isPaid,
+            isAdultsOnly = response.data?.event?.adultsOnly,
+            selectedStartDateTime = parseUtcStringToLocalDateTime(response.data?.event?.startDate!!),
+            selectedEndDateTime = parseUtcStringToLocalDateTime(response.data?.event?.endDate!!),
+            selectedKeywords = response.data?.event?.keywords,
+            selectedCategory = response.data?.event?.category,
+            maxNumberOfAttendees = response.data?.event?.maxNumberOfAttendees,
+            host = User(
+                displayName = response.data?.event?.host?.displayName!!,
+                userId = response.data?.event?.host?.userId!!,
+                photoUrl = response.data?.event?.host?.photoUrl?.let { Uri.parse(it) },
+                creationDate = parseUtcStringToLocalDateTime(response.data?.event?.host?.creationDate!!),
+                lastSeenOnline = parseUtcStringToLocalDateTime(response.data?.event?.host?.lastSeenOnline!!)
+            ),
+            lastUpdatedDate = parseUtcStringToLocalDateTime(response.data?.event?.lastUpdateDate!!),
+            photos = response.data?.event?.images ?: emptyList()
+        )
     }
 
     override suspend fun createEvent(event: Event): Int {
 
         Log.d("ApolloEventClient", "createEvent: $event")
-        //call the apollo client to create the event
         val apolloClient = ApolloClient.Builder()
             .serverUrl(BuildConfig.API_URL)
             .build()
-        //create response variable to hold the response from the server
         val response = apolloClient.mutation(
             CreateEventMutation(
                 title = event.title!!,
@@ -92,7 +134,8 @@ class EventRepository : IEventRepository {
                     lng = event.location.geoLocation?.lng!!
                 ),
                 category = event.selectedCategory!!,
-                keywords = event.selectedKeywords!!,
+                keywords = event.selectedKeywords!! as List<String>,
+                description = event.description!!,
             )
         ).execute()
         Log.d("ApolloEventClient", "createEvent: ${response.data?.createEvent?.event?.id}")
--- app/src/main/java/io/github/viabachelora23michaelkutaibakasper/bprapp/data/repository/IEventRepository.kt
+++ app/src/main/java/io/github/viabachelora23michaelkutaibakasper/bprapp/data/repository/IEventRepository.kt
@@ -4,7 +4,7 @@ import io.github.viabachelora23michaelkutaibakasper.bprapp.data.domain.Event
 
 interface IEventRepository {
     suspend fun getEvents(): List<Event>
-    suspend fun getEvent(eventId: String): Event
+    suspend fun getEvent(eventId: Int): Event
     suspend fun createEvent(event: Event): Int
 
     suspend fun getKeywords(): List<String>
