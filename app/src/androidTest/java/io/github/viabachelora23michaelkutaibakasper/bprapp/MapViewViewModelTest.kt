f03f8eb4ad4baa6bd346c20c59b632662389b495
Kasper-S-Jensen <kasperscoop@gmail.com>
2023-12-01 15:34:36 +0100

    add test of MapView vm

---
+++ app/src/androidTest/java/io/github/viabachelora23michaelkutaibakasper/bprapp/MapViewViewModelTest.kt
@@ -0,0 +1,40 @@
+package io.github.viabachelora23michaelkutaibakasper.bprapp
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import io.github.viabachelora23michaelkutaibakasper.bprapp.data.domain.Event
+import io.github.viabachelora23michaelkutaibakasper.bprapp.mocks.FakeEventRepository
+import io.github.viabachelora23michaelkutaibakasper.bprapp.ui.screens.events.MapViewViewModel
+import io.github.viabachelora23michaelkutaibakasper.bprapp.ui.screens.events.createevent.CreateEventViewModel
+import junit.framework.TestCase
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MapViewViewModelTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+    private val repository = FakeEventRepository()
+
+
+    @Test
+    fun getEvents_returnsListOfEvents() {
+        val viewModel =
+            MapViewViewModel(repository = repository)
+
+        // Verify the initial state
+        TestCase.assertEquals(emptyList<Event>(), viewModel.eventList.value)
+
+        composeTestRule.runOnIdle {
+            viewModel.getEvents()
+        }
+        composeTestRule.waitForIdle()
+
+        // Verify the updated state
+        TestCase.assertEquals(true, viewModel.eventList.value.size == 3)
+    }
+
+
+}
