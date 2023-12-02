8b1110711bb1ce8b75df335248fc27a654a7882f
Kasper-S-Jensen <kasperscoop@gmail.com>
2023-12-02 15:34:34 +0100

    clean up

--- app/src/main/java/io/github/viabachelora23michaelkutaibakasper/bprapp/MainActivity.kt
+++ app/src/main/java/io/github/viabachelora23michaelkutaibakasper/bprapp/MainActivity.kt
@@ -18,10 +18,12 @@ import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
@@ -32,10 +34,14 @@ import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -110,7 +116,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val repository: IEventRepository = EventRepository()
                     val createEventViewModel: CreateEventViewModel =
                         viewModel()
                     val eventDetailsViewModel: EventDetailsViewModel = viewModel()
@@ -118,6 +123,7 @@ class MainActivity : ComponentActivity() {
                     val navController: NavHostController = rememberNavController()
                     val scope = rememberCoroutineScope()
                     val snackbarHostState = remember { SnackbarHostState() }
+
 
                     Scaffold(
                         snackbarHost = {
@@ -203,6 +209,7 @@ class MainActivity : ComponentActivity() {
                             )
                         }
                     ) { innerPadding ->
+
                         fun showSnackBar() {
                             scope.launch {
                                 val result = snackbarHostState
@@ -210,7 +217,7 @@ class MainActivity : ComponentActivity() {
                                         message = "Event created successfully",
                                         actionLabel = "See event",
                                         // Defaults to SnackbarDuration.Short
-                                        duration = SnackbarDuration.Indefinite,
+                                        duration = SnackbarDuration.Short,
                                         withDismissAction = true
                                     )
                                 when (result) {
@@ -224,7 +231,7 @@ class MainActivity : ComponentActivity() {
                                     }
 
                                     SnackbarResult.Dismissed -> {
-                                        /* Handle snackbar dismissed */
+
                                     }
                                 }
                             }
@@ -233,6 +240,8 @@ class MainActivity : ComponentActivity() {
                             Log.d("MainActivity", "onCreate: event created")
                             showSnackBar()
                         }
+
+
                         NavHost(
                             navController = navController,
                             startDestination = BottomNavigationScreens.Map.name,
