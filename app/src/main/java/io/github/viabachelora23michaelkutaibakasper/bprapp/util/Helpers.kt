424a9a42b92d3361ef1e5599c2068f9308a7f121
Kasper-S-Jensen <kasperscoop@gmail.com>
2023-12-03 14:17:10 +0100

    modify formatter for edge cases

--- app/src/main/java/io/github/viabachelora23michaelkutaibakasper/bprapp/util/Helpers.kt
+++ app/src/main/java/io/github/viabachelora23michaelkutaibakasper/bprapp/util/Helpers.kt
@@ -1,12 +1,33 @@
 package io.github.viabachelora23michaelkutaibakasper.bprapp.util
 
+import java.time.Instant
+import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 import java.util.Locale
 
 fun parseUtcStringToLocalDateTime(utcString: String): LocalDateTime {
-    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    val formatter: DateTimeFormatter = when {
+        utcString.contains('.') && utcString.split('.').last().length == 9 -> {
+            // If the string contains a dot and the last section has three digits, use the pattern for three digits
+            DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+        }
+        utcString.contains('.') && utcString.split('.').last().length == 11 -> {
+            // If the string contains a dot and the last section has six digits, use the pattern for six digits
+            DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSXXX")
+        }
+        utcString.contains('.') && utcString.split('.').last().length == 12 -> {
+            // If the string contains a dot and the last section has six digits, use the pattern for six digits
+            DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSXXX")
+        }
+
+        else -> {
+            // If no dot or an unexpected format, use a fallback pattern
+            DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX")
+        }
+    }
     return LocalDateTime.parse(utcString, formatter)
 }
 
@@ -17,4 +38,16 @@ fun DisplayFormattedTime(
         .ofLocalizedDateTime(FormatStyle.SHORT)
         .withLocale(Locale.UK)
     return dateTime.format(formatter)
+}
+
+fun convertUtcMillisecondsToFormattedDate(
+    utcMilliseconds: Long?
+): LocalDate {
+
+    return Instant
+        .ofEpochMilli(utcMilliseconds!!)
+        .atOffset(
+            ZoneOffset.UTC
+        )
+        .toLocalDate()
 }
