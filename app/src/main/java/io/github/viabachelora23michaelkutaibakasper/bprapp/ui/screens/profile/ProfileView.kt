3f5187cdcbccf9ca9d31204c252214dddf1a4a75
Kasper-S-Jensen <kasperscoop@gmail.com>
2023-12-06 14:21:06 +0100

    add initial dialog of rating

--- app/src/main/java/io/github/viabachelora23michaelkutaibakasper/bprapp/ui/screens/profile/ProfileView.kt
+++ app/src/main/java/io/github/viabachelora23michaelkutaibakasper/bprapp/ui/screens/profile/ProfileView.kt
@@ -1,7 +1,5 @@
 package io.github.viabachelora23michaelkutaibakasper.bprapp.ui.screens.profile
 
-import android.util.Log
-import android.widget.RatingBar
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -16,19 +14,32 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -37,31 +48,33 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import io.github.viabachelora23michaelkutaibakasper.bprapp.R
 import io.github.viabachelora23michaelkutaibakasper.bprapp.data.sign_in.AuthenticationClient
 import io.github.viabachelora23michaelkutaibakasper.bprapp.data.sign_in.IAuthenticationClient
 import io.github.viabachelora23michaelkutaibakasper.bprapp.ui.navigation.BottomNavigationScreens
-import io.github.viabachelora23michaelkutaibakasper.bprapp.ui.screens.events.EventListItem
 import io.github.viabachelora23michaelkutaibakasper.bprapp.ui.screens.events.LoadingScreen
-import io.github.viabachelora23michaelkutaibakasper.bprapp.ui.screens.events.MapViewViewModel
 import io.github.viabachelora23michaelkutaibakasper.bprapp.util.DisplayFormattedTime
+import io.github.viabachelora23michaelkutaibakasper.bprapp.util.roundToNearestHalf
 import java.time.LocalDateTime
+import kotlin.math.roundToInt
 
 
+@ExperimentalMaterial3Api
 @Composable
 fun ProfileScreen(navController: NavController, viewModel: ProfileViewModel) {
     val authenticationClient: IAuthenticationClient = AuthenticationClient()
     val user by viewModel.user
+    var sliderValue by remember { mutableFloatStateOf(0f) }
+
     val isLoading by viewModel.isLoading
+    val openDialog = remember { mutableStateOf(false) }
     val events by viewModel.eventList.collectAsState()
     val errorFetchingEvents by viewModel.errorFetchingEvents
     val launcher = authenticationClient.signIn(onAuthComplete = { result ->
@@ -101,7 +114,6 @@ fun ProfileScreen(navController: NavController, viewModel: ProfileViewModel) {
                 Spacer(modifier = Modifier.height(16.dp))
             }
             if (user!!.displayName != null) {
-
                 Text(
                     text = user!!.displayName!!,
                     textAlign = TextAlign.Center,
@@ -140,7 +152,9 @@ fun ProfileScreen(navController: NavController, viewModel: ProfileViewModel) {
             ) {
                 LazyColumn {
                     items(events) { event ->
-                        Column(modifier = Modifier.fillMaxWidth().clickable { navController.navigate("${BottomNavigationScreens.EventDetails.name}/${event.eventId}") }) {
+                        Column(modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable { navController.navigate("${BottomNavigationScreens.EventDetails.name}/${event.eventId}") }) {
                             Row(
                                 Modifier.padding(4.dp),
                                 verticalAlignment = Alignment.CenterVertically
@@ -197,11 +211,84 @@ fun ProfileScreen(navController: NavController, viewModel: ProfileViewModel) {
             }
         }
         Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { openDialog.value = true }) {
+            Text(text = "Rate the event")
+
+        }
         Button(onClick = {
             authenticationClient.signOut()
             viewModel.user.value = null
         }) {
             Text(text = "Sign out")
+        }
+
+        if (openDialog.value) {
+            Dialog(onDismissRequest = { openDialog.value = false }) {
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(250.dp)
+                        .padding(16.dp),
+                    shape = RoundedCornerShape(16.dp),
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        Text(text = "Please rate the event from 1-5", fontWeight = FontWeight.Bold)
+                        Text(
+                            text = "Rating: $sliderValue.",
+                            modifier = Modifier.padding(16.dp),
+                        )
+
+                        Slider(
+                            modifier = Modifier.padding(4.dp),
+                            value = sliderValue,
+                            onValueChange = { sliderValue = it.roundToNearestHalf() },
+                            colors = SliderDefaults.colors(
+                                thumbColor = MaterialTheme.colorScheme.primary,
+                                activeTrackColor = MaterialTheme.colorScheme.primary,
+                                inactiveTrackColor = MaterialTheme.colorScheme.secondaryContainer,
+                            ),
+                            steps = 10,
+                            valueRange = 0f..5f,
+                            thumb = {
+                                Icon(
+                                    imageVector = Icons.Filled.Star,
+                                    tint = Color.Yellow,
+                                    contentDescription = null
+                                )
+                            }
+                        )
+
+
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Center,
+                        ) {
+                            TextButton(
+                                onClick = { openDialog.value = false },
+                                modifier = Modifier.padding(8.dp),
+                            ) {
+                                Text("Dismiss")
+                            }
+                            TextButton(
+                                onClick = {
+                                    openDialog.value = false
+                                },
+                                modifier = Modifier.padding(8.dp),
+                            ) {
+                                Text("Confirm")
+                            }
+                        }
+                    }
+                }
+            }
+
         }
 
     }
@@ -221,6 +308,7 @@ private fun RefreshButton(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun PreviewProfileScreen() {
