f4f112085450cbc1820ca36ef16ba919376b3efa
Kasper-S-Jensen <kasperscoop@gmail.com>
2023-12-07 14:43:50 +0100

    fetch recommendations in viewmodel

--- app/src/main/java/io/github/viabachelora23michaelkutaibakasper/bprapp/ui/screens/recommendations/RecommendationsViewModel.kt
+++ app/src/main/java/io/github/viabachelora23michaelkutaibakasper/bprapp/ui/screens/recommendations/RecommendationsViewModel.kt
@@ -7,9 +7,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import io.github.viabachelora23michaelkutaibakasper.bprapp.data.domain.MinimalEvent
 import io.github.viabachelora23michaelkutaibakasper.bprapp.data.repository.EventRepository
 import io.github.viabachelora23michaelkutaibakasper.bprapp.data.repository.IEventRepository
 import io.github.viabachelora23michaelkutaibakasper.bprapp.data.sign_in.FireStoreClient
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class RecommendationsViewModel(repository: IEventRepository = EventRepository()) : ViewModel() {
@@ -21,6 +24,10 @@ class RecommendationsViewModel(repository: IEventRepository = EventRepository())
     private var _keywords = mutableStateOf(emptyList<String>())
     private var _categories = mutableStateOf(emptyList<String>())
     private val firestoreClient = FireStoreClient()
+    val isLoading = mutableStateOf(false)
+    val errorFetchingEvents = mutableStateOf(false)
+    private val _recommendationsList = MutableStateFlow<List<MinimalEvent>>(emptyList())
+    val recommendationsList = _recommendationsList.asStateFlow()
 
     fun setSurveyFilled(hostId: String) {
         isSurveyFilled.value = firestoreClient.SetSurveyFilled(hostId)
@@ -76,6 +83,27 @@ class RecommendationsViewModel(repository: IEventRepository = EventRepository())
             }
         }
         return emptyList()
+    }
+
+    fun getRecommendations(userId: String, numberOfEvents: Int) {
+        viewModelScope.launch {
+            errorFetchingEvents.value = false
+            try {
+                isLoading.value = true
+                val events =
+                    eventRepository.getReccommendations(
+                        userId = userId,
+                        numberOfEvents = numberOfEvents
+                    )
+                _recommendationsList.value = events
+                Log.d("recommendationsViewmodel", "getevents: $events")
+                isLoading.value = false
+            } catch (e: Exception) {
+                Log.d("recommendationsViewViewModel", "error message: ${e.printStackTrace()}")
+                isLoading.value = false
+                errorFetchingEvents.value = true
+            }
+        }
     }
 
 }
