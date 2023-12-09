a43c07c75f55322b0acaa0485b3f497cf524269f
Kasper-S-Jensen <kasperscoop@gmail.com>
2023-12-09 11:15:03 +0100

    implement custom greyscale method

--- app/src/main/java/io/github/viabachelora23michaelkutaibakasper/bprapp/util/Helpers.kt
+++ app/src/main/java/io/github/viabachelora23michaelkutaibakasper/bprapp/util/Helpers.kt
@@ -1,5 +1,13 @@
 package io.github.viabachelora23michaelkutaibakasper.bprapp.util
 
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.DrawModifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.drawscope.ContentDrawScope
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -47,3 +55,20 @@ fun Float.roundToNearestHalf(): Float {
 fun localDateTimeToUTCLocalDateTime(localDateTime: LocalDateTime): LocalDateTime? {
     return localDateTime.atOffset(ZoneOffset.UTC).toLocalDateTime()
 }
+
+class GreyScaleModifier : DrawModifier {
+    override fun ContentDrawScope.draw() {
+        val saturationMatrix = androidx.compose.ui.graphics.ColorMatrix().apply { setToSaturation(0.1f) }
+        val saturationFilter = ColorFilter.colorMatrix(saturationMatrix)
+        val paint = Paint().apply {
+            colorFilter = saturationFilter
+        }
+        drawIntoCanvas {
+            it.saveLayer(Rect(0f, 0f, size.width, size.height), paint)
+            drawContent()
+            it.restore()
+        }
+    }
+}
+
+fun Modifier.greyScale() = this.then(GreyScaleModifier())
