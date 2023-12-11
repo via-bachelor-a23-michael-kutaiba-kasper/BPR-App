651df62e696fda96958bd341e51f54676c8d80d8
Kasper-S-Jensen <kasperscoop@gmail.com>
2023-12-11 17:23:41 +0100

    add experiencehistory domain entity

--- app/src/main/java/io/github/viabachelora23michaelkutaibakasper/bprapp/data/domain/Experience.kt
+++ app/src/main/java/io/github/viabachelora23michaelkutaibakasper/bprapp/data/domain/Experience.kt
@@ -1,9 +1,16 @@
 package io.github.viabachelora23michaelkutaibakasper.bprapp.data.domain
 
+import java.time.LocalDateTime
+
 
 data class Experience(
     val totalExp: Int,
     val level: Int,
     val minExp: Int,
     val maxExp: Int,
+)
+
+data class ExperienceHistory(
+    val exp: Int,
+    val date: LocalDateTime
 )
