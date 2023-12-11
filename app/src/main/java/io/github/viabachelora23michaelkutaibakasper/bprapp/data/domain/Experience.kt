2ed84d0abb5fd0ad1b1f20605b8472725124d924
Kasper-S-Jensen <kasperscoop@gmail.com>
2023-12-11 16:59:30 +0100

    create domain entities

---
+++ app/src/main/java/io/github/viabachelora23michaelkutaibakasper/bprapp/data/domain/Experience.kt
@@ -0,0 +1,9 @@
+package io.github.viabachelora23michaelkutaibakasper.bprapp.data.domain
+
+
+data class Experience(
+    val totalExp: Int,
+    val level: Int,
+    val minExp: Int,
+    val maxExp: Int,
+)
